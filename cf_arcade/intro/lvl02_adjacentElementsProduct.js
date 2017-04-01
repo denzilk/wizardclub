2bf962c96c09a45058c3be50edf776b526a0ede8
denzilk <github@densetsu.net>
2017-04-01 16:56:32 -0400

    Create lvl02_adjacentElementsProduct.js

---
+++ cf_arcade/intro/lvl02_adjacentElementsProduct.js
@@ -0,0 +1,44 @@
+/*
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Input/Output
+
+[time limit] 4000ms (js)
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The largest product of adjacent elements.
+
+
+https://codefights.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m
+*/
+
+
+function adjacentElementsProduct(inputArray) {
+    var prod, highest = Number.NEGATIVE_INFINITY;
+    for(var i=0; i<inputArray.length-1; i++){
+        prod = inputArray[i] * inputArray[i+1];
+
+        if(prod > highest){
+            highest = prod;
+        }
+    }
+    return highest;
+}
+
+
+
