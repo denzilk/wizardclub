41f8fb3d13a9c659b7d58c20cc5156b3bf9c6b10
denzilk <github@densetsu.net>
2017-04-01 16:47:34 -0400

    Create lvl04_areSimilar.js

---
+++ cf_arcade/intro/lvl04_areSimilar.js
@@ -0,0 +1,72 @@
+/*
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+Given two arrays, check whether they are similar.
+
+Example
+
+For A = [1, 2, 3] and B = [1, 2, 3], the output should be
+areSimilar(A, B) = true.
+
+The arrays are equal, no need to swap any elements.
+
+For A = [1, 2, 3] and B = [2, 1, 3], the output should be
+areSimilar(A, B) = true.
+
+We can obtain B from A by swapping 2 and 1 in B.
+
+For A = [1, 2, 2] and B = [2, 1, 1], the output should be
+areSimilar(A, B) = false.
+
+Any swap of any two elements either in A or in B won't make A and B equal.
+
+Input/Output
+
+[time limit] 4000ms (js)
+[input] array.integer A
+
+Array of integers.
+
+Guaranteed constraints:
+3 ≤ A.length ≤ 105,
+1 ≤ A[i] ≤ 1000.
+
+[input] array.integer B
+
+Array of integers of the same length as A.
+
+Guaranteed constraints:
+B.length = A.length,
+1 ≤ B[i] ≤ 1000.
+
+[output] boolean
+
+true if A and B are similar, false otherwise.
+
+
+
+
+https://codefights.com/arcade/intro/level-4/xYXfzQmnhBvEKJwXP
+*/
+
+
+
+function areSimilar(A, B) {
+    var deltas = 0;
+    var diffs = {};
+    for(var i=0; i<A.length; i++){
+        if(A[i] == B[i]){
+            continue;
+        }
+        deltas++;
+        if(deltas > 2){
+            return false;
+        }
+        diffs[A[i]]=true;
+        diffs[B[i]]=true;        
+        if(Object.keys(diffs).length > 2){
+            return false;
+        }
+    }
+    return true;
+}
