c3337fbc0f317d406195118d8085934981f16d6d
denzilk <github@densetsu.net>
2017-04-02 14:15:12 -0400

    Create arithmeticExpression.js

---
+++ cf_arcade/core/02_at_the_crossroads/arithmeticExpression.js
@@ -0,0 +1,56 @@
+/*
+Consider an arithmetic expression of the form A#B=C. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+
+Example
+
+For A = 2, B = 3 and C = 5, the output should be
+arithmeticExpression(A, B, C) = true.
+
+We can replace # with a + to obtain 2 + 3 = 5, so the answer is true.
+
+For A = 8, B = 2 and C = 4, the output should be
+arithmeticExpression(A, B, C) = true.
+
+We can replace # with a / to obtain 8 / 2 = 4, so the answer is true.
+
+For A = 8, B = 3 and C = 2, the output should be
+arithmeticExpression(A, B, C) = false.
+
+8 + 3 = 11 ≠ 2;
+8 - 3 = 5 ≠ 2;
+8 * 3 = 24 ≠ 2;
+8 / 3 = 2.(6) ≠ 2.
+So the answer is false.
+
+Input/Output
+
+[time limit] 4000ms (js)
+[input] integer A
+
+Guaranteed constraints:
+1 ≤ A ≤ 10.
+
+[input] integer B
+
+Guaranteed constraints:
+1 ≤ B ≤ 10.
+
+[input] integer C
+
+Guaranteed constraints:
+0 ≤ C ≤ 10.
+
+[output] boolean
+
+true if the desired replacement is possible, false otherwise.
+
+https://codefights.com/arcade/code-arcade/at-the-crossroads/QrCSNQWhnQoaK9KgK
+*/
+
+
+function arithmeticExpression(A, B, C) {
+    return ((A + B) == C) ||
+        ((A - B) == C) ||
+        ((A * B) == C) ||
+        ((B && (A / B)) == C);
+}
